777914f80160690b502b4b8e6bd7a53d14524010
Yi Liu <yiliu4@habana.ai>
2025-03-01 16:00:03 +0200

    fix config check
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- vllm/model_executor/model_loader/loader.py
+++ vllm/model_executor/model_loader/loader.py
@@ -480,8 +480,7 @@ class DefaultModelLoader(BaseModelLoader):
 
     def _need_patch_inc_fp8_kvcache_(self, config):
         user_pass_inc_as_quantization = (
-            config.quantization is not None
-            and "inc" in self.model_config.quantization
+            config.quantization is not None and "inc" in config.quantization
         )
         return (
             config.cache_config.cache_dtype == "fp8_inc"
