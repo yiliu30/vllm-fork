780579da8351b389a5cdf1d13048fda5dd873cfe
yi <yi>
2025-03-05 11:19:18 +0800

    add ray debug
    Signed-off-by: yi <yi>

--- vllm/executor/ray_utils.py
+++ vllm/executor/ray_utils.py
@@ -273,7 +273,17 @@ def initialize_ray_cluster(
                      ignore_reinit_error=True,
                      num_gpus=parallel_config.world_size)
     else:
-        ray.init(address=ray_address, ignore_reinit_error=True)
+        ray.init(
+            address=ray_address, 
+            ignore_reinit_error=True,
+            runtime_env={
+                "env_vars": {
+                    "RAY_DEBUG": "1"
+                    }, 
+                }
+        )
+        
+
 
     if parallel_config.placement_group:
         # Placement group is already set.
