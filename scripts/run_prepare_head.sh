bbfe5d56d09e0a0dc00dce41da1b987d758babae
Yi Liu <yiliu4@habana.ai>
2025-03-06 05:10:44 +0200

    update source files
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- scripts/run_prepare_head.sh
+++ scripts/run_prepare_head.sh
@@ -10,9 +10,12 @@ else
     echo "Config file not found!"
     exit 1
 fi
+
 # reset the environment and start the ray cluster
 BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
 source "$BASH_DIR"/head_node_source.sh
 export QUANT_CONFIG=$INC_MEASURE_CONFIG_FILENAME
 
 ray start --head --port $RAY_CLUSTER_PORT
+
+set +x
