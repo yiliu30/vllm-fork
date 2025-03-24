306d4ad86361d91f65e2bf9d5df47b0f56638056
Tony Lin <tony.lin@intel.com>
2025-03-24 15:08:35 +0800

    add script for 128k/160k model len w/ input len up to model len

---
+++ scripts/multi_nodes_source160k_graphed.sh
@@ -0,0 +1,87 @@
+#! /bin/bash
+
+# set -x
+
+BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+ray stop --force
+
+# DO NOT change unless you fully undersand its purpose
+export HABANA_VISIBLE_DEVICES="ALL"
+export VLLM_MLA_DISABLE_REQUANTIZATION=1
+export PT_HPU_ENABLE_LAZY_COLLECTIVES="true"
+export VLLM_RAY_DISABLE_LOG_TO_DRIVER="1"
+export RAY_IGNORE_UNHANDLED_ERRORS="1"
+export PT_HPU_WEIGHT_SHARING=0
+export HABANA_VISIBLE_MODULES="0,1,2,3,4,5,6,7"
+export PT_HPUGRAPH_DISABLE_TENSOR_CACHE=1
+
+export VLLM_MOE_N_SLICE=8
+export VLLM_EP_SIZE=16
+
+block_size=128
+# DO NOT change ends...
+
+# memory footprint tunning params
+export VLLM_GPU_MEMORY_UTILIZATION=0.8
+export VLLM_GRAPH_RESERVED_MEM=0.2
+export VLLM_GRAPH_PROMPT_RATIO=0
+# params
+max_num_batched_tokens=163840
+max_num_seqs=16
+input_min=128
+input_max=163840
+output_max=163840
+
+unset VLLM_PROMPT_BS_BUCKET_MIN VLLM_PROMPT_BS_BUCKET_STEP VLLM_PROMPT_BS_BUCKET_MAX
+unset VLLM_PROMPT_SEQ_BUCKET_MIN VLLM_PROMPT_SEQ_BUCKET_STEP VLLM_PROMPT_SEQ_BUCKET_MAX
+unset VLLM_DECODE_BS_BUCKET_MIN VLLM_DECODE_BS_BUCKET_STEP VLLM_DECODE_BS_BUCKET_MAX
+unset VLLM_DECODE_BLOCK_BUCKET_MIN VLLM_DECODE_BLOCK_BUCKET_STEP VLLM_DECODE_BLOCK_BUCKET_MAX
+
+#export VLLM_SKIP_WARMUP=true
+unset VLLM_SKIP_WARMUP
+#export PT_HPU_RECIPE_CACHE_CONFIG=/mnt/jarvis1-disk3/HF_models/hlin76/Cache128K_64K_BS16_STEP1024_STEP16,false,16384
+unset PT_HPU_RECIPE_CACHE_CONFIG
+
+#export PT_HPU_METRICS_FILE=/root/metricslog.json
+#export PT_HPU_METRICS_DUMP_TRIGGERS=process_exit,metric_change
+unset PT_HPU_METRICS_FILE PT_HPU_METRICS_DUMP_TRIGGERS
+
+
+# !!!!!!!!!!!!!!!!!!!! set bucketing !!!!!!!!!!!!!
+max_model_len=$max_num_batched_tokens
+
+prompt_bs_min=1
+prompt_bs_step=$(( $max_num_seqs > 32 ? 32 : $max_num_seqs ))
+prompt_bs_max=$(( $max_num_seqs > 64 ? 64 : $max_num_seqs ))
+export VLLM_PROMPT_BS_BUCKET_MIN=${VLLM_PROMPT_BS_BUCKET_MIN:-$prompt_bs_min}
+export VLLM_PROMPT_BS_BUCKET_STEP=${VLLM_PROMPT_BS_BUCKET_STEP:-$prompt_bs_step}
+export VLLM_PROMPT_BS_BUCKET_MAX=${VLLM_PROMPT_BS_BUCKET_MAX:-$prompt_bs_max}
+
+prompt_seq_step=1024
+prompt_seq_min=128
+prompt_seq_max=$max_model_len
+export VLLM_PROMPT_SEQ_BUCKET_MIN=${VLLM_PROMPT_SEQ_BUCKET_MIN:-$prompt_seq_min}
+export VLLM_PROMPT_SEQ_BUCKET_STEP=${VLLM_PROMPT_SEQ_BUCKET_STEP:-$prompt_seq_step}
+export VLLM_PROMPT_SEQ_BUCKET_MAX=${VLLM_PROMPT_SEQ_BUCKET_MAX:-$prompt_seq_max}
+
+decode_bs_min=1
+decode_bs_step=$(( $max_num_seqs > 32 ? 32 : $max_num_seqs ))
+decode_bs_max=$max_num_seqs
+export VLLM_DECODE_BS_BUCKET_MIN=${VLLM_DECODE_BS_BUCKET_MIN:-$decode_bs_min}
+export VLLM_DECODE_BS_BUCKET_STEP=${VLLM_DECODE_BS_BUCKET_STEP:-$decode_bs_step}
+export VLLM_DECODE_BS_BUCKET_MAX=${VLLM_DECODE_BS_BUCKET_MAX:-$decode_bs_max}
+
+decode_block_min=128
+decode_block_step=128
+block_size=128
+decode_block_max=$(( ((max_num_seqs * max_model_len / block_size) > 128) ? (max_num_seqs * max_model_len / block_size) : 128 ))
+export VLLM_DECODE_BLOCK_BUCKET_MIN=${VLLM_DECODE_BLOCK_BUCKET_MIN:-$decode_block_min}
+export VLLM_DECODE_BLOCK_BUCKET_STEP=${VLLM_DECODE_BLOCK_BUCKET_STEP:-$decode_block_step}
+export VLLM_DECODE_BLOCK_BUCKET_MAX=${VLLM_DECODE_BLOCK_BUCKET_MAX:-$decode_block_max}
+
+echo " environments are reseted "
+
+env | grep VLLM
+
+#python3 -m vllm.entrypoints.openai.api_server --host 0.0.0.0 --port 8838 --block-size 128 --model /mnt/jarvis1-disk3/HF_models/DeepSeek-R1-BF16-w8afp8-static-no-ste-converted/ --device hpu --dtype bfloat16 --tensor-parallel-size 16 --kv-cache-dtype fp8_inc --trust-remote-code  --max-model-len 163840 --max-num-seqs 16 --max-num-batched-tokens 163840 --use-padding-aware-scheduling --use-v2-block-manager --distributed_executor_backend ray --gpu_memory_utilization 0.8 --disable-log-requests
