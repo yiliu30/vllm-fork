9e52c70bee30c8324f510ad7ef90de70735f2ccc
Yi <yi4.liu@intel.com>
2025-04-09 20:21:21 +0800

    update prof code
    Signed-off-by: Yi <yi4.liu@intel.com>

--- scripts/benchmark_serving_range.sh
+++ scripts/benchmark_serving_range.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 
-model_path=/models/Qwen2-72B-Instruct
+model_path=/mnt/disk5/yiliu4/DeepSeek-R1-G2-dynamic
+model_path=/mnt/disk5/yiliu4/DeepSeek-R1-G2-static
 host=127.0.0.1
-port=30001
-input=1000
-output=500
+port=8688
+input=2048
+output=2048
 ratio=0.8
-num_prompts=1000
+num_prompts=96
 request_rate=inf
+max_concurrency=16
 
 model_name=$( echo $model_path | awk -F/ '{print $NF}' )
-echo "Benchmarking ${model_path} for vllm server '${host}:${port}' with input_max=${input}, output_max=${output}, ratio=${ratio}, num_prompts=${num_prompts}, request_rate=${request_rate}"
+echo "Benchmarking ${model_path} for vllm server '${host}:${port}' with input_max=${input}, output_max=${output}, ratio=${ratio}, num_prompts=${num_prompts}, request_rate=${request_rate}, max_concurrency=${max_concurrency}"
 
-log_name=benchmark_serving_${model_name}_random_in-${input}_out-${output}_ratio-${ratio}_rate-${request_rate}_prompts-${num_prompts}_$(date +%F-%H-%M-%S)
+log_name=benchmark_serving_static_dmoe_inc_fp8mla_fp8kv_${model_name}_random_in-${input}_out-${output}_ratio-${ratio}_rate-${request_rate}_prompts-${num_prompts}_${max_concurrency}_$(date +%F-%H-%M-%S)
 
 python ../benchmarks/benchmark_serving.py \
     --backend vllm \
@@ -28,6 +30,7 @@ python ../benchmarks/benchmark_serving.py \
     --request-rate $request_rate \
     --seed 0 \
     --save-result \
+    --max-concurrency $max_concurrency \
     --result-filename "${log_name}".json \
     --ignore-eos \
     |& tee "${log_name}".log 2>&1
