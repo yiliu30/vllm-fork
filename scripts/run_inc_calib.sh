4cf490465c765994842443124c3d90d8b1dc13aa
Yi Liu <yiliu4@habana.ai>
2025-05-07 10:53:16 +0300

    fix model path
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- scripts/run_inc_calib.sh
+++ scripts/run_inc_calib.sh
@@ -1,4 +1,4 @@
-DEFAULT_MODEL_PATH="/mnt/disk3/yiliu4/DeepSeek-R1-G2-INC-424-Converter207/"
+DEFAULT_MODEL_PATH="/mnt/disk3/DeepSeek-R1-G2-INC"
 FP8_MODEL_PATH="${1:-$DEFAULT_MODEL_PATH}"
 
 QUANT_CONFIG_FILE="scripts/quant_configs/inc_measure_with_fp8kv_config.json"
