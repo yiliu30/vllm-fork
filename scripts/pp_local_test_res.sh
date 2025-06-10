dabdb2528a13dd871cfd8b70ec468732c3146f62
yiliu30 <yi4.liu@intel.com>
2025-06-10 03:35:23 +0000

    add 2k/2k test res
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

---
+++ scripts/pp_local_test_res.sh
@@ -0,0 +1,51 @@
+# Traffic request rate: inf
+# Burstiness factor: 1.0 (Poisson process)
+# Maximum request concurrency: 32
+# 100%|████████████████████████████████████████████████████████████████████████| 32/32 [02:30<00:00,  4.71s/it]
+# ============ Serving Benchmark Result ============
+# Successful requests:                     32        
+# Benchmark duration (s):                  150.87    
+# Total input tokens:                      65536     
+# Total generated tokens:                  65536     
+# Request throughput (req/s):              0.21      
+# Output token throughput (tok/s):         434.40    
+# Total Token throughput (tok/s):          868.80    
+# ---------------Time to First Token----------------
+# Mean TTFT (ms):                          7279.56   
+# Median TTFT (ms):                        7099.88   
+# P99 TTFT (ms):                           10999.46  
+# -----Time per Output Token (excl. 1st token)------
+# Mean TPOT (ms):                          70.14     
+# Median TPOT (ms):                        70.23     
+# P99 TPOT (ms):                           72.63     
+# ---------------Inter-token Latency----------------
+# Mean ITL (ms):                           70.14     
+# Median ITL (ms):                         68.15     
+# P99 ITL (ms):                            72.47     
+# ==================================================
+
+# Traffic request rate: inf
+# Burstiness factor: 1.0 (Poisson process)
+# Maximum request concurrency: 32
+# 100%|███████████████████████████████████████████████████████████████| 32/32 [03:18<00:00,  6.21s/it]
+# ============ Serving Benchmark Result ============
+# Successful requests:                     32        
+# Benchmark duration (s):                  198.68    
+# Total input tokens:                      65536     
+# Total generated tokens:                  65536     
+# Request throughput (req/s):              0.16      
+# Output token throughput (tok/s):         329.86    
+# Total Token throughput (tok/s):          659.72    
+# ---------------Time to First Token----------------
+# Mean TTFT (ms):                          6535.45   
+# Median TTFT (ms):                        6272.98   
+# P99 TTFT (ms):                           10709.26  
+# -----Time per Output Token (excl. 1st token)------
+# Mean TPOT (ms):                          93.85     
+# Median TPOT (ms):                        93.99     
+# P99 TPOT (ms):                           96.35     
+# ---------------Inter-token Latency----------------
+# Mean ITL (ms):                           93.85     
+# Median ITL (ms):                         91.76     
+# P99 ITL (ms):                            97.38     
+# ==================================================
