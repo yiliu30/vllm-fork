145a1b3010d1dd1f1928f2d0dffe2a4fb918498d
yingjieh <yingjie.han@intel.com>
2025-06-11 15:53:36 +0800

    enable qwenvl on 1.21.0 (#1400)
    Test：
    VLLM_SKIP_WARMUP=true PT_HPU_LAZY_MODE=1 python
    examples/offline_inference/vision_language.py -m qwen_vl

--- vllm/model_executor/models/qwen_vl.py
+++ vllm/model_executor/models/qwen_vl.py
@@ -745,7 +745,27 @@ class QwenVLForConditionalGeneration(QWenBaseModel, SupportsPP, SupportsLoRA,
 
         vision_embeddings = self._process_image_input(image_input)
         return vision_embeddings
-
+    
+    # utils.merge_multimodal_embeddings has problem in graph mode, so we
+    # implement a custom version here.
+    def _merge_multimodal_embeddings(
+        self,
+        input_ids: torch.Tensor,
+        hidden_states: torch.Tensor,
+        multimodal_embeddings: MultiModalEmbeddings,
+        image_pad_id: int,
+    ) -> torch.Tensor:
+        """
+        Merge multimodal embeddings with the input embeddings.
+        """
+        batch_size, seq_length, hidden_size = hidden_states.shape
+        hidden_states = hidden_states.reshape(-1, hidden_size)
+        multimodal_embeddings = multimodal_embeddings.reshape(-1, hidden_size)
+        hidden_states[input_ids.reshape(-1) == image_pad_id] = multimodal_embeddings
+        hidden_states = hidden_states.reshape(batch_size, seq_length,
+                                                hidden_size)
+        return hidden_states
+        
     def get_input_embeddings(
         self,
         input_ids: torch.Tensor,
@@ -754,7 +774,7 @@ class QwenVLForConditionalGeneration(QWenBaseModel, SupportsPP, SupportsLoRA,
         inputs_embeds = self.transformer.get_input_embeddings(input_ids)
 
         if multimodal_embeddings is not None:
-            inputs_embeds = merge_multimodal_embeddings(
+            inputs_embeds = self._merge_multimodal_embeddings(
                 input_ids, inputs_embeds, multimodal_embeddings,
                 self.transformer.visual.image_pad_id)
 
