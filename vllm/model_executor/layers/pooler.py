b8ebf29fe6d7edd6aa702d39a36bda3c585a7ccd
Zheng Wang <zheng.w.wang@intel.com>
2025-06-17 16:20:10 +0800

    Fix warmup error when pooling (embedding and clasification) is running (#1442)
    by checking if pooling_param is None
    
    @czhu15  @yangulei @ranzhejiang

--- vllm/model_executor/layers/pooler.py
+++ vllm/model_executor/layers/pooler.py
@@ -241,6 +241,7 @@ class PoolerHead(nn.Module):
         dimensions_list = [
             pooling_param.dimensions
             for _, pooling_param in pooling_metadata.seq_groups
+            if pooling_param is not None
         ]
         if any(d is not None for d in dimensions_list):
             # change the output dimension
