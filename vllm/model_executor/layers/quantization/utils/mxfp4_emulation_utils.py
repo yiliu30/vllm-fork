cb7387b3eed1882b32846c960c167acd0922f471
yiliu30 <yi4.liu@intel.com>
2025-07-03 23:54:20 -0400

    format code
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- vllm/model_executor/layers/quantization/utils/mxfp4_emulation_utils.py
+++ vllm/model_executor/layers/quantization/utils/mxfp4_emulation_utils.py
@@ -1,44 +1,52 @@
 import torch
 import vllm.envs as envs
-from vllm.scalar_type import scalar_types
 
+from torchao.prototype.mx_formats.mx_tensor import (
+    to_mx,
+    to_dtype,
+    ScaleCalculationMode,
+)
 
-
-from torchao.prototype.mx_formats.mx_tensor import to_mx, to_dtype, ScaleCalculationMode
 
 def run_mxfp4_emulations(
-        x: torch.Tensor,
-        weight: torch.Tensor,
-        weight_scale: torch.Tensor,
+    x: torch.Tensor,
+    weight: torch.Tensor,
+    weight_scale: torch.Tensor,
 ):
     group_size = 32
     if envs.VLLM_DISABLE_INPUT_QDQ:
         x_dq = x
     else:
         # quantize input to (FP4 and interleaved block scale)
-        input_scale, x_q = to_mx(data_hp=x,
-                                elem_dtype="fp4_e2m1",
-                                block_size=group_size,
-                                scaling_mode=ScaleCalculationMode.RCEIL,
-                                pack_fp6=False)
+        input_scale, x_q = to_mx(
+            data_hp=x,
+            elem_dtype="fp4_e2m1",
+            block_size=group_size,
+            scaling_mode=ScaleCalculationMode.RCEIL,
+            pack_fp6=False,
+        )
 
         # dequantize input
-        x_dq = to_dtype(data_lp=x_q, 
-                        scale_e8m0=input_scale,
-                        elem_dtype="fp4_e2m1",
-                        block_size=group_size,
-                        target_dtype=x.dtype,
-                        use_fp4_custom_triton_dequant_kernel=False,
-                        pack_fp6=False)
+        x_dq = to_dtype(
+            data_lp=x_q,
+            scale_e8m0=input_scale,
+            elem_dtype="fp4_e2m1",
+            block_size=group_size,
+            target_dtype=x.dtype,
+            use_fp4_custom_triton_dequant_kernel=False,
+            pack_fp6=False,
+        )
 
     # dequantize weight
-    w_dq = to_dtype(data_lp=weight, 
-                    scale_e8m0=weight_scale,
-                    elem_dtype="fp4_e2m1",
-                    block_size=group_size,
-                    target_dtype=x.dtype,
-                    use_fp4_custom_triton_dequant_kernel=False,
-                    pack_fp6=False)
+    w_dq = to_dtype(
+        data_lp=weight,
+        scale_e8m0=weight_scale,
+        elem_dtype="fp4_e2m1",
+        block_size=group_size,
+        target_dtype=x.dtype,
+        use_fp4_custom_triton_dequant_kernel=False,
+        pack_fp6=False,
+    )
 
     # matmul
     out = torch.matmul(x_dq, w_dq.t())
