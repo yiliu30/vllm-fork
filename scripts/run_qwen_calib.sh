e31a6d8a70a3a3882942b88a0199399f56c68b62
yiliu30 <yi4.liu@intel.com>
2025-07-08 05:39:28 +0000

    update res
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- scripts/run_qwen_calib.sh
+++ scripts/run_qwen_calib.sh
@@ -88,12 +88,35 @@ mkdir -p ${output_dir}
 
 
 
-QUANT_CONFIG=${quant_file_path} \
-PT_HPU_LAZY_MODE=1 \
-VLLM_SKIP_WARMUP=true \
-PT_HPU_ENABLE_LAZY_COLLECTIVES=true \
-PT_HPU_WEIGHT_SHARING=0 \
-lm_eval --model vllm \
-  --model_args "pretrained=${model_path},tensor_parallel_size=${tp_size},max_model_len=4096,max_num_seqs=128,gpu_memory_utilization=0.8,use_v2_block_manager=True,dtype=bfloat16,max_gen_toks=2048,disable_log_stats=True,quantization=inc" \
-  --tasks gsm8k --batch_size 128 --log_samples  --output_path ${output_dir} --show_config 2>&1 | tee ${output_dir}/log_post.txt
+# QUANT_CONFIG=${quant_file_path} \
+# PT_HPU_LAZY_MODE=1 \
+# VLLM_SKIP_WARMUP=true \
+# PT_HPU_ENABLE_LAZY_COLLECTIVES=true \
+# PT_HPU_WEIGHT_SHARING=0 \
+# lm_eval --model vllm \
+#   --model_args "pretrained=${model_path},tensor_parallel_size=${tp_size},max_model_len=4096,max_num_seqs=128,gpu_memory_utilization=0.8,use_v2_block_manager=True,dtype=bfloat16,max_gen_toks=2048,disable_log_stats=True,quantization=inc" \
+#   --tasks gsm8k --batch_size 128 --log_samples  --output_path ${output_dir} --show_config 2>&1 | tee ${output_dir}/log_post.txt
+
+
+# -bf16 model
+# vllm (pretrained=/mnt/disk9/yiliu7/deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=1,max_model_len=4096,max_num_seqs=128,gpu_memory_utilization=0.8,use_v2_block_manager=True,dtype=bfloat16,max_gen_toks=2048,disable_log_stats=True), gen_kwargs: (None), limit: None, num_fewshot: None, batch_size: 128
+# |Tasks|Version|     Filter     |n-shot|  Metric   |   |Value |   |Stderr|
+# |-----|------:|----------------|-----:|-----------|---|-----:|---|-----:|
+# |gsm8k|      3|flexible-extract|     5|exact_match|↑  |0.9166|±  |0.0076|
+# |     |       |strict-match    |     5|exact_match|↑  |0.8469|±  |0.0099|
+
 # -bf16 kv before post
+# vllm (pretrained=/mnt/disk9/yiliu7/deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=1,max_model_len=4096,max_num_seqs=128,gpu_memory_utilization=0.8,use_v2_block_manager=True,dtype=bfloat16,max_gen_toks=2048,disable_log_stats=True,quantization=inc), gen_kwargs: (None), limit: None, num_fewshot: None, batch_size: 128
+# |Tasks|Version|     Filter     |n-shot|  Metric   |   |Value |   |Stderr|
+# |-----|------:|----------------|-----:|-----------|---|-----:|---|-----:|
+# |gsm8k|      3|flexible-extract|     5|exact_match|↑  |0.9325|±  |0.0069|
+# |     |       |strict-match    |     5|exact_match|↑  |0.8954|±  |0.0084|
+
+# -fp8 kv after post
+# vllm (pretrained=/mnt/disk9/yiliu7/deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=1,max_model_len=4096,max_num_seqs=128,gpu_memory_utilization=0.8,use_v2_block_manager=True,dtype=bfloat16,max_gen_toks=2048,disable_log_stats=True,quantization=inc,kv_cache_dtype=fp8_inc), gen_kwargs: (None), limit: None, num_fewshot: None, batch_size: 128
+# |Tasks|Version|     Filter     |n-shot|  Metric   |   |Value |   |Stderr|
+# |-----|------:|----------------|-----:|-----------|---|-----:|---|-----:|
+# |gsm8k|      3|flexible-extract|     5|exact_match|↑  |0.9325|±  |0.0069|
+# |     |       |strict-match    |     5|exact_match|↑  |0.8954|±  |0.0084|
+
+
