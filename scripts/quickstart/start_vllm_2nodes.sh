7a3194e963b57c8229e9014366e6b0541647b287
yiliu30 <yi4.liu@intel.com>
2025-07-15 12:42:19 +0000

    update example
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- scripts/quickstart/start_vllm_2nodes.sh
+++ scripts/quickstart/start_vllm_2nodes.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-export MODEL_PATH=/mnt/disk9/hf_models/models--moonshotai--Kimi-K2-Instruct/
+export MODEL_PATH=/mnt/disk9/hf_models/Kimi-K2-Instruct-G2/
+export MODEL_PATH=/mnt/disk7/yiliu4/DeepSeek-R1-0528-G2-2nd/
+export PT_HPU_LAZY_MODE=1
+export VLLM_SKIP_WARMUP=true
+export VLLM_LOGGING_LEVEL=DEBUG
+export RAY_DEDUP_LOGS=0
 
 max_num_seqs=32
 max_num_batched_tokens=4096
 block_size=128
 VLLM_GPU_MEMORY_UTILIZATION=0.6
 
+
 python -m vllm.entrypoints.openai.api_server \
-    --host 192.168.1.101 \
     --port 8688 \
     --model $MODEL_PATH \
-    --tensor-parallel-size 16 \
+    --tensor-parallel-size 8 \
     --max-num-seqs $max_num_seqs \
     --max-num-batched-tokens $max_num_batched_tokens \
     --disable-log-requests \
     --dtype bfloat16 \
-    --kv-cache-dtype fp8_inc \
     --use-v2-block-manager \
     --num_scheduler_steps 1\
     --block-size $block_size \
--- scripts/quickstart/start_worker.sh
+++ scripts/quickstart/start_worker.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 source ./set_worker_node.sh
 ray start --address='10.239.128.244:8850'
+
+sleep 10
+
+ray status
