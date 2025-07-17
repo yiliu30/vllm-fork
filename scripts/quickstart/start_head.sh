3e84c87b87973c6c91e7025f5dabded33bed0f44
yiliu30 <yi4.liu@intel.com>
2025-07-17 03:03:17 +0000

    update head cmd
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- scripts/quickstart/start_head.sh
+++ scripts/quickstart/start_head.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ./set_head_node.sh
-ray start --head --node-ip-address=10.239.128.244 --port=8850
+ray start --head --node-ip-address=10.239.128.244 --port=8850  --num-cpus 160
