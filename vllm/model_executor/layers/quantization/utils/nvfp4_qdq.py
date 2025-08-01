95135b614bd878e63811f9b0d559421958370bac
Yi Liu <yi4.liu@intel.com>
2025-08-01 19:39:53 +0800

    Revert "Nvfp4 static gs (#61)" (#63)
    This reverts commit c4ef8139e9c8af5ddbb90214b83df720fd9030df.

--- vllm/model_executor/layers/quantization/utils/nvfp4_qdq.py
+++ vllm/model_executor/layers/quantization/utils/nvfp4_qdq.py
@@ -265,28 +265,16 @@ def nvfp4_quantize(
     return out_scales, data_lp
 
 
-def to_nvfp4(x, x_global_scale=None, do_pack=True):
-    if x_global_scale is None:
-        tensor_amax = torch.max(torch.abs(x))
-        per_tensor_scale = per_tensor_amax_to_scale(tensor_amax)
-        x_global_scale = per_tensor_scale
+def to_nvfp4(x, do_pack=True):
+    tensor_amax = torch.max(torch.abs(x))
+    per_tensor_scale = per_tensor_amax_to_scale(tensor_amax)
     out_scales, data_lp = nvfp4_quantize(
         data_hp=x,
         block_size=16,
-        per_tensor_scale=x_global_scale,
+        per_tensor_scale=per_tensor_scale,
         do_pack=do_pack,
     )
     return data_lp, out_scales, per_tensor_scale
-
-
-def to_nvfp4_with_gs(x, x_global_scale, do_pack=True):
-    out_scales, data_lp = nvfp4_quantize(
-        data_hp=x,
-        block_size=16,
-        per_tensor_scale=x_global_scale,
-        do_pack=do_pack,
-    )
-    return data_lp, out_scales
 
 
 def dequant_nvfp4(
@@ -329,11 +317,11 @@ def check_nan(x):
     return torch.isnan(x).any() or torch.isinf(x).any()
 
 
-def qdq_nvfp4(x, x_global_scale=None):
+def qdq_nvfp4(x):
     if envs.VLLM_DISABLE_INPUT_QDQ:
         return x
 
-    data_lp, x_scale = to_nvfp4(x, x_global_scale, do_pack=False)
+    data_lp, x_scale, x_global_scale = to_nvfp4(x, do_pack=False)
     x_dq = dequant_nvfp4(
         data_lp,
         x_scale,
@@ -343,35 +331,6 @@ def qdq_nvfp4(x, x_global_scale=None):
     )
     return x_dq
 
-
-def qdq_nvfp4(x, x_global_scale=None):
-    if envs.VLLM_DISABLE_INPUT_QDQ:
-        return x
-
-    data_lp, x_scale = to_nvfp4_with_gs(x, x_global_scale, do_pack=False)
-    x_dq = dequant_nvfp4(
-        data_lp,
-        x_scale,
-        x_global_scale,
-        original_dtype=x.dtype,
-        packed=False,
-    )
-    return x_dq
-
-
-def qdq_nvfp4_with_gs(x, x_global_scale):
-    if envs.VLLM_DISABLE_INPUT_QDQ:
-        return x
-
-    data_lp, x_scale = to_nvfp4_with_gs(x, x_global_scale, do_pack=False)
-    x_dq = dequant_nvfp4(
-        data_lp,
-        x_scale,
-        x_global_scale,
-        original_dtype=x.dtype,
-        packed=False,
-    )
-    return x_dq
 
 class NVFP4Linear(torch.nn.Module):
     def __init__(self, in_features, out_features):
